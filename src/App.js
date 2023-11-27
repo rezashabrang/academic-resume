54fd4fee158d5647cc80d9273fe9c7227f07960d
rezashabrang <rezashabrang.m@gmail.com>
2023-11-27 23:05:48 +0330

    ACADEMIC BASELINE

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
 import React, { useState } from 'react';
 import AcademicResume from './academic';
-import JobResume from './job';
 import './App.css';
 import './index.css';
 
 
-
 const App = () => {
-  const [showAcademicResume, setShowAcademicResume] = useState(false);
-  const [showJobResume, setShowJobResume] = useState(true);
-
-  const handleAcademicButtonClick = () => {
-    setShowAcademicResume(true);
-    setShowJobResume(false); // Hide job resume
-  };
-
-  const handleJobButtonClick = () => {
-    setShowJobResume(true);
-    setShowAcademicResume(false); // Hide academic resume
-  };
-
   return (
-    <>
-      {(!showAcademicResume && !showJobResume) && (
-        <div className="intro-container">
-          <div className="intro-content">
-            <button className='neon-button' onClick={handleAcademicButtonClick}>Academic Resume</button>
-            <button className='neon-button' onClick={handleJobButtonClick}>Job Resume</button>
-          </div>
-        </div>
-      )}
-
-      {showAcademicResume && <AcademicResume />}
-      {showJobResume && <JobResume />}
-    </>
-
+    <AcademicResume />
   );
 };
 
